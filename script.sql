f69a0ad51947998def0bee0ac682613fed207725
jacquehettel <jacque.tidwell@gmail.com>
2018-12-17 15:19:51 -0500

    Added notes to SQL scripts.

--- script.sql
+++ script.sql
@@ -5,3 +5,21 @@ SELECT *
     WHERE country LIKE 'United%';
 
 /*I saw that my country was listed as United States, so I modified my query to find my city.*/
+
+SELECT *
+	FROM city_list
+    WHERE country LIKE 'United States';
+
+/*I then saw that the closest city to where I live in Athens, Georgia, was listed: Atlanta.*/
+/*I expanded the city_data column facet to see that the column I wanted to filter on was city.*/
+SELECT *
+	FROM city_data
+    WHERE city = 'Atlanta' AND country = 'United States';
+
+/*This yielded my city level data.*/
+/*I saved this output as city_data_atlanta_us.csv*/
+
+/*I then modified my search to download all of the global data.*/
+SELECT *
+	FROM global_data;
+	/*I saved this output as global_data.csv*/
